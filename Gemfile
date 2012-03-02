c1b6205a49be3576a7a539ae2dbca3e87d0f3149
Vincent Paca <vincent.paca@gmail.com>
2012-03-03 01:28:48 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2'
+gem 'sqlite3'
+gem 'devise'
+gem 'cancan'
+gem 'gon'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'turn', '~> 0.8.3', :require => false
+  gem 'shoulda'
+  gem 'factory_girl'
+  gem 'simplecov', require: false
+end
+
+group :development do
+  gem 'rails-dev-tweaks'
+  gem 'nifty-generators'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Echo::Application.config.secret_token = 'ffb3f5157770dd354f86cb1224df1ab2becfe202e96179839e780f62ca4596689a89f4ab8d658a3d82c55d694985aa8b5d2aeade00ec6cfc8026cfa07ca6f83b'
